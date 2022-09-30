4313697a859058fcd2393955e799e3e25b33f5e1
Cem Celebi <ccelebi@protonmail.com>
2022-09-30 16:10:05 +0200

    Fix typo in output

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -1,14 +1,14 @@
 output "service_arn" {
   description = "The App Runner Service ARN"
-  value       = module.test.service_arn
+  value       = module.kata.service_arn
 }
 
 output "service_url" {
   description = "The App Runner Service URL"
-  value       = module.test.service_url
+  value       = module.kata.service_url
 }
 
 output "service_status" {
   description = "The App Runner Service Status"
-  value       = module.test.service_status
+  value       = module.kata.service_status
 }
